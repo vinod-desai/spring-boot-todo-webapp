afa58d704422600af8168ae473004a4f7a66fd5f
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-13 21:35:31 +0530

    Create Error.java

---
+++ src/main/java/com/java/example/model/Error.java
@@ -0,0 +1,15 @@
+package com.java.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Error {
+    private String code;
+    private String message;
+}
