d59d46ccb3ac8dcb63378db9fdfbe901c9ddcdfb
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-13 21:33:40 +0530

    Create TodoNotFoundException.java

---
+++ src/main/java/com/java/example/exception/TodoNotFoundException.java
@@ -0,0 +1,14 @@
+package com.java.example.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoNotFoundException extends RuntimeException {
+    private String message;
+}
