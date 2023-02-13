d58501c9a0b19737510f207e3844d351483e96cc
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-13 21:38:12 +0530

    Create TodoAppApplication.java

---
+++ src/main/java/com/java/example/todoapp/TodoAppApplication.java
@@ -0,0 +1,32 @@
+package com.java.example.todoapp;
+
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication()
+@EnableMongoRepositories(basePackages = "com.java.example.dao")
+@ComponentScan(basePackages = "com.java.example.*")
+public class TodoAppApplication {
+
+    public static void main(String[] args) {
+        ApplicationInsights.attach();
+        SpringApplication.run(TodoAppApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+
+}
