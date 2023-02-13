5725b3cbfd9e79d0c2cdfb5205d94cd3da86378a
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-13 21:32:32 +0530

    Create TodoRepository.java

---
+++ src/main/java/com/java/example/dao/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.java.example.dao;
+
+import com.java.example.model.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
