ad02bc07208ebcf42d4e001084d2bcc8747805d5
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-15 20:51:04 +0530

    Create todo.ts

---
+++ client/src/app/todo-list/todo.ts
@@ -0,0 +1,9 @@
+export interface Todo {
+    id: string;
+    description: string;
+    status: string;
+    createdByUserName: string;
+    createdOnDateTime: string;
+    modifiedByUserName: string;
+    modifiedOnDateTime: string;
+}
