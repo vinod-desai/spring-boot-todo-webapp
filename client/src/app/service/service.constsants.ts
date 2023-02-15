cc790945b61cc59037bc4cfedf173c8276f4cb19
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-15 20:49:09 +0530

    Create service.constsants.ts

---
+++ client/src/app/service/service.constsants.ts
@@ -0,0 +1,4 @@
+// ApiBaseUrl http://localhost:4200/api when we run angular app
+// in another server and/or port
+// export const ApiBaseUrl: string = "http://localhost:4200/api";
+export const ApiBaseUrl: string = "/api";
