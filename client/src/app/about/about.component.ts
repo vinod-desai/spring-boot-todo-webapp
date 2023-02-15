55842664473e6b6f27c10bf8c17c26e096c7d57b
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-02-15 20:45:50 +0530

    Update about.component.ts

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,1 +1,25 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AboutService } from '../service/about.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit, OnDestroy  {
+
+  private subscriptions = new Subscription();
+
+  constructor(private readonly aboutService: AboutService) { }
+
+  ngOnInit(): void {
+    this.subscriptions.add(this.aboutService.getAbout().subscribe());
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+}
 
